d0616bba6271fecdfd6c6277bf9f1d346013e695
Ashish Garg <ashish2.kumar@trantorinc.com>
2018-12-15 00:24:09 +0530

    code refactored

--- lib/simple_notifications/base.rb
+++ lib/simple_notifications/base.rb
@@ -1,6 +1,6 @@
 module SimpleNotifications
   module Base
-    mattr_accessor :notified_flag, :entity, :sender, :receivers, :is_sender_receiver_associated
+    mattr_accessor :notified_flag, :sender, :receivers
 
     #Getter to check if model is enabled with notifications
     def notified?
@@ -21,21 +21,33 @@ module SimpleNotifications
       @@receivers.to_s.in?(self.reflections.keys) || @@receivers.kind_of?(ActiveRecord::Relation) || @@receivers.kind_of?(ActiveRecord::Base) || self.methods(false)
     end
 
-    # Starts the notification functionality on the Model.
+    # Add this method to any model and it starts the notification functionality on the Model.
+    # Expects two parameters :sender and :receiver.
     def notify(options = {})
       @@sender = options[:sender]
       @@receivers = options[:receivers]
       raise 'SimpleNotifications::SenderReceiversError' unless notification_validated?
 
-      define_associations
+      # Define association for sender model
+      @@sender.class.class_eval do
+        has_many :sent_notifications, class_name: 'SimpleNotifications::Record', as: :sender
+      end unless @@sender.class == NilClass
+
+      # Define association for receiver model
+      @@receivers.collect(&:class).flatten.uniq.each do |base|
+        base.class_eval do
+          has_many :deliveries, class_name: 'SimpleNotifications::Delivery', as: :receiver
+          has_many :received_notifications, through: :deliveries, source: :simple_notification
+        end unless base.class == NilClass
+      end
 
       self.class_eval do
-        # --- Associations --- #
+        # Define association for the notified model
         has_many :notifications, class_name: 'SimpleNotifications::Record', as: :entity
         has_many :notifiers, through: :notifications, source: :sender, source_type: @@sender.class.name
         has_many :notificants, through: :notifications, source: :receivers
 
-        # --- Callbacks --- #
+        # Callbacks for notified model
         after_create_commit :create_notification
 
         private
@@ -52,19 +64,6 @@ module SimpleNotifications
       end
 
       @@notified_flag = true
-    end
-
-    def define_associations
-      @@sender.class.class_eval do
-        has_many :sent_notifications, class_name: 'SimpleNotifications::Record', as: :sender
-      end unless @@sender.class == NilClass
-
-      @@receivers.collect(&:class).flatten.uniq.each do |base|
-        base.class_eval do
-          has_many :deliveries, class_name: 'SimpleNotifications::Delivery', as: :receiver
-          has_many :received_notifications, through: :deliveries, source: :simple_notification
-        end unless base.class == NilClass
-      end
     end
   end
 end
