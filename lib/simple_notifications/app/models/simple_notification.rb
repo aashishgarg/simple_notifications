0b9427306599e78ecb8c3abec23d8ed1d6f0cf2f
Ashish Garg <ashish2.kumar@trantorinc.com>
2018-12-14 15:24:31 +0530

    Notification receiver association fixed

--- lib/simple_notifications/app/models/simple_notification.rb
+++ lib/simple_notifications/app/models/simple_notification.rb
@@ -7,17 +7,19 @@ module SimpleNotifications
     belongs_to :entity, polymorphic: true
     has_many :deliveries, class_name: 'SimpleNotifications::Delivery', inverse_of: :simple_notification, foreign_key: :simple_notification_id,
              dependent: :destroy
+    # FIXME source_type fixed here as USER, need to be made dynamic
+    has_many :receivers, through: :deliveries, source: :receiver, source_type: 'User'
 
     # --- validations --- #
     validates :message, presence: true, length: {minimum: 1, maximum: 199}
 
     # --- Callbacks --- #
-    after_commit :after_actions, on: :create
+    after_create_commit :after_actions
 
     private
 
     def after_actions
-
+      puts
     end
   end
 end
